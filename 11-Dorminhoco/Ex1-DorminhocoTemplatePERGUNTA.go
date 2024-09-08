f664469e301bef6425ee8f9ef3cc6080ef7bdedb
fcmfreitas <franciscocmfreitas@gmail.com>
2024-09-08 14:33:59 -0300

    progresso no dorminhoco

--- 11-Dorminhoco/Ex1-DorminhocoTemplatePERGUNTA.go
+++ 11-Dorminhoco/Ex1-DorminhocoTemplatePERGUNTA.go
@@ -13,31 +13,110 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const NJ = 5           // numero de jogadores
 const M = 4            // numero de cartas
 
-type carta string      // carta é um strirng
+type carta string      // carta é um string
 
 var ch [NJ]chan carta  // NJ canais de itens tipo carta  
 
-func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta, ... ) {
+func embaralhaCartas() [5][5]carta {
+	// Cria o baralho com 21 cartas (4 de cada letra e uma carta extra)
+	baralho := []carta{
+		"10", "10", "10", "10",
+		"J", "J", "J", "J",
+		"Q", "Q", "Q", "Q",
+		"K", "K", "K", "K",
+		"A", "A", "A", "A",
+		"@", // carta coringa
+	}
+
+	// Embaralha o baralho
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(baralho), func(i, j int) { baralho[i], baralho[j] = baralho[j], baralho[i] })
+
+	// Cria a matriz 5x5
+	var matriz [5][5]carta
+
+	// Preenche a matriz com as cartas embaralhadas
+	for i := 0; i < 5; i++ {
+		if i == 0 {
+			for j := 0; j < 5; j++ {
+				index := i*5 + j
+				if index < len(baralho) {
+					matriz[i][j] = baralho[index]
+				}
+			}
+		} else{
+			for j := 0; j < 4; j++ {
+				index := i*4 + j
+				if index < len(baralho) {
+					matriz[i][j] = baralho[index]
+				}
+			}
+		}
+			
+	}
+
+	// Imprime a matriz
+	imprimirMatriz(matriz)
+
+	return matriz
+}
+
+func imprimirMatriz(matriz [5][5]carta) {
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Print(matriz[i][j], " ")
+		}
+		fmt.Println()
+	}
+}
+
+func encontrarCartaMaisDiferente(mao []carta) carta {
+	
+	frequencias := make(map[carta]int)
+	
+	for i := 0; i < len(mao); i++ {
+		cartaAtual := mao[i]
+		frequencias[cartaAtual]++
+	}
+	var cartaMenosFrequente carta
+	minFrequencia := len(mao) + 1
+	
+	for carta, frequencia := range frequencias {
+		if frequencia < minFrequencia {
+			minFrequencia = frequencia
+			cartaMenosFrequente = carta
+		}
+	}
+	
+	return cartaMenosFrequente
+}
+
+func jogador(id int, mesa chan carta, cartasIniciais []carta ) {
+
 	mao := cartasIniciais    // estado local - as cartas na mao do jogador
-	nroDeCartas := M         // quantas cartas ele tem 
-    cartaRecebida := " "     // carta recebida é vazia
 
 	for {
-		if // tenho que jogar
-			{
+		if len(mao) == 5{
+			
 			fmt.Println(id, " joga") // escreve seu identificador
-			// escolhe alguma carta para passar adiante ...
-			// guarda carta que entrou 
-			// manda carta escolhida o proximo
-			out <- cartaParaSair        
+			
+			cartaDescartada := encontrarCartaMaisDiferente(mao) //seleciona a melhor carta para passar
+			mesa <- cartaDescartada
+			mao = removerCarta(mao, cartaDescartada)
+			
+			fmt.Printf("Jogador %d descartou: %v\n", id, cartaDescartada)
+
 		} else {  
-			// ...
-			cartaRecebida := <-in   // recebe carta na entrada
+		
+			mao = append(mao, <-mesa)
+			// recebe carta da mesa
 			//  ...
 			// e se alguem bate ?
 		}
@@ -45,16 +124,30 @@ func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta, ... 
 }
 
 func main() {
-	for i := 0; i < NJ; i++ {
-		ch[i] = make(chan struct{})
-	}
-	// cria um baralho com NJ*M cartas
-	for i := 0; i < NJ; i++ {
-		// escolhe aleatoriamente (tira) cartas do baralho, passa cartas para jogador
-		go jogador(i, ch[i], ch[(i+1)%N], cartasEscolhidas , ...) // cria processos conectados circularmente
-	}
 	
-	<-make(chan struct{}) // bloqueia
+	matriz := embaralhaCartas() // cria baralho e embaralha
+	canalMesa := make(chan carta, 1) // cria o canal em que será passada a carta
+
+	// distribui as cartas e cria os jogadores
+	for i := 0; i < NJ; i++ {
+		var cartasIniciais []carta
+		
+		if i == 0{
+			for j := 0; j < M+1; j++ {
+				cartasIniciais = append(cartasIniciais, matriz[i][j])
+			}
+		} else {
+			for j := 0; j < M; j++ {
+				cartasIniciais = append(cartasIniciais, matriz[i][j])
+			}
+		}
+
+		go jogador(i, canalMesa, cartasIniciais)
+	}
+
+	// Mantém o programa rodando
+	<-make(chan struct{}) // Bloqueia
+	
 }
 
 
